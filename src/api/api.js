5903e7d1d39b1da88f631f6766eda4e10c9b1968
3Lsome <luckyllemos@gmail.com>
2024-06-14 08:42:25 +0800

    Update update api.js and logoutComponent

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-export default axios.create({
+ const axiosAPI = axios.create({
    baseURL: 'http://localhost:80/',
 //    baseURL: 'http://10.8.7.183/',
     //  baseURL: 'https://w4.core-access.com/',
     headers: { 'token' : 'cCW7PW2CRotxuALrBuMob5lXgVhY4xo'  }
 });
+
+
+export default axiosAPI;
--- src/components/LogoutComponent/Logout.vue
+++ src/components/LogoutComponent/Logout.vue
@@ -24,7 +24,7 @@ export default {
     methods:{
         logOut(){
             this.loggedIn = 'no'
-            document.location.href = "/App";
+            document.location.href = "https://foxcyloxcy.github.io/call-recording/";
         },
     }
 
