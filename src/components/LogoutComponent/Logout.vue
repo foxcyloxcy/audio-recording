09e8d5ed5c20756fa44ca694f8ece95c6c6fe26d
3Lsome <luckyllemos@gmail.com>
2024-06-18 20:10:50 +0800

    Update fix bug for Logout

--- src/components/LogoutComponent/Logout.vue
+++ src/components/LogoutComponent/Logout.vue
@@ -24,7 +24,7 @@ export default {
     methods:{
         logOut(){
             this.loggedIn = 'no'
-            document.location.href = "https://foxcyloxcy.github.io/call-recording/";
+            document.location.href = "https://foxcyloxcy.github.io/audio-recording/";
         },
     }
 
